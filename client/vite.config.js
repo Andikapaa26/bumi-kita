6021f33f82be42f3de3242c71298bf64b16e4424
Andika Pangestu <andikapangestu26@upi.edu>
2024-01-02 08:58:00 +0700

    update6

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '/@/': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     chunkSizeWarningLimit: 16000000000000,
   },
