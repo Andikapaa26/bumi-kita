dfa2eefc4433b97ae06f3e0bc5c375bc0440a83a
Andika Pangestu <andikapangestu26@upi.edu>
2024-01-02 09:27:46 +0700

    update10

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/webpack';
-import path from 'path'; // tambahkan baris ini
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '/@/': path.resolve(__dirname, 'src'),
-    },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],  // Sesuaikan dengan dependensi Anda
   },
   build: {
     chunkSizeWarningLimit: 16000000000000,
-    rollupOptions: {
-      plugins: [
-        svgr(), // Enable SVG handling
-      ],
-    },
   },
-})
+});
